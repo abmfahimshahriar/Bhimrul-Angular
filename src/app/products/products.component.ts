e8954b535bfec7f31d308e10275b3c9ca33b4973
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-08-29 04:27:35 +0600

    show products on home page and filter products by category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,38 @@
+import { CategoryService } from './../category.service';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/products';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  category: string;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+    productService: ProductService) { 
+    productService.getAll()
+      .pipe(
+        switchMap(products => {
+          this.products = products;
+          return this.route.queryParamMap;
+        })
+      )
+      .subscribe(params => {
+        this.category = params.get('category');
+        
+        this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) :
+        this.products;
+      });
+    
   }
 
 }
